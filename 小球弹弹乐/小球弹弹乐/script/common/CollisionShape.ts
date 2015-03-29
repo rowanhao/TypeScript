a03a58128c704c22fb44ef9b304e4e1e145c3fd1
rowanhao <rowanhao1@163.com>
2015-03-29 19:04:10 +0800

    版本2.0

---
+++ 小球弹弹乐/小球弹弹乐/script/common/CollisionShape.ts
@@ -0,0 +1,12 @@
+﻿
+
+class CollisionShape {
+   direction: Vector2;
+   speed: number;
+   constructor(dir: Vector2 = new Vector2(0, 1), sp: number = 1) {
+       this.direction = dir;
+       this.speed = sp;
+   }
+   draw(canvas: CanvasRenderingContext2D) { }
+   move() { }
+}
