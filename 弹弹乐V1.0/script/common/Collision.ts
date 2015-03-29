e62d4ada6445ab34dd16db86bfb8f8f1f86f490d
rowanhao <rowanhao1@163.com>
2015-03-29 18:22:34 +0800

    升级为V2.0

---
+++ 弹弹乐V1.0/script/common/Collision.ts
@@ -0,0 +1,10 @@
+﻿
+
+function Cir2Cir(a: Circle, b: Circle):boolean {
+    var len = new Vector2(a.x - b.x, a.y - b.y);
+    if (len.length2 <= (a.r + b.r) * (a.r + b.r)){
+        return true;
+    } else {
+        return false;
+    }
+}
